a4110a5ce0b625c709240153cae5fc43b3cf3195
Mohit Rohda <163033754+mohitrohda@users.noreply.github.com>
2025-07-20 23:26:41 +0530

    updating app.py

--- app.py
+++ app.py
@@ -1,73 +1,28 @@
 import streamlit as st
 import pickle
-import requests
 
-def fetch_poster(movie_id):
-     url = "https://api.themoviedb.org/3/movie/{}?api_key=c7ec19ffdd3279641fb606d19ceb9bb1&language=en-US".format(movie_id)
-     data=requests.get(url)
-     data=data.json()
-     poster_path = data['poster_path']
-     full_path = "https://image.tmdb.org/t/p/w500/"+poster_path
-     return full_path
+# Load data
+movies = pickle.load(open("Movie_Recommender/movies_list.pkl", 'rb'))
+similarity = pickle.load(open("Movie_Recommender/similarity.pkl", 'rb'))
+movie_titles = movies['title'].values
 
-movies = pickle.load(open("movies_list.pkl", 'rb'))
-similarity = pickle.load(open("similarity.pkl", 'rb'))
-movies_list=movies['title'].values
-
-st.header("Movie Recommender System")
-
-import streamlit.components.v1 as components
-
-
-
-imageUrls = [
-    fetch_poster(1632),
-    fetch_poster(299536),
-    fetch_poster(17455),
-    fetch_poster(2830),
-    fetch_poster(429422),
-    fetch_poster(9722),
-    fetch_poster(13972),
-    fetch_poster(240),
-    fetch_poster(155),
-    fetch_poster(598),
-    fetch_poster(914),
-    fetch_poster(255709),
-    fetch_poster(572154)
-   
-    ]
-
-
-selectvalue=st.selectbox("Select movie from dropdown", movies_list)
-
+# Recommend movies (without posters)
 def recommend(movie):
-    index=movies[movies['title']==movie].index[0]
-    distance = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda vector:vector[1])
-    recommend_movie=[]
-    recommend_poster=[]
-    for i in distance[1:6]:
-        movies_id=movies.iloc[i[0]].id
-        recommend_movie.append(movies.iloc[i[0]].title)
-        recommend_poster.append(fetch_poster(movies_id))
-    return recommend_movie, recommend_poster
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_titles = []
+    for i in distances[1:6]:  # top 5 recommendations
+        recommended_titles.append(movies.iloc[i[0]].title)
+    return recommended_titles
 
+# Streamlit UI
+st.set_page_config(page_title="Movie Recommender", layout="wide")
+st.title("🔍 Movie Recommender System (No Posters)")
+selected_movie = st.selectbox("🎬 Choose a movie:", movie_titles)
 
-
-if st.button("Show Recommend"):
-    movie_name, movie_poster = recommend(selectvalue)
-    col1,col2,col3,col4,col5=st.columns(5)
-    with col1:
-        st.text(movie_name[0])
-        st.image(movie_poster[0])
-    with col2:
-        st.text(movie_name[1])
-        st.image(movie_poster[1])
-    with col3:
-        st.text(movie_name[2])
-        st.image(movie_poster[2])
-    with col4:
-        st.text(movie_name[3])
-        st.image(movie_poster[3])
-    with col5:
-        st.text(movie_name[4])
-        st.image(movie_poster[4])
+if st.button("Show Recommendations"):
+    with st.spinner("Fetching..."):
+        recommendations = recommend(selected_movie)
+        st.subheader("Recommended Movies:")
+        for movie in recommendations:
+            st.write("👉", movie)
